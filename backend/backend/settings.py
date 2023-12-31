78c63a74733a5fbfc6d708a4cc4c0e91bd6bc1cc
Viktor Gorbach <viktor.gorbatch@yandex.ru>
2023-12-31 20:00:48 +0300

    add setup file 4 flake8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,12 +60,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': '/data/db.sqlite3',
 #    }
-#}
+# }
 
 DATABASES = {
     'default': {
